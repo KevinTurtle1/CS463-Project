6620d34f0011fde9e6c47fe792135e69efdca2f3
Kevin Vuong <kvuong@pdx.edu>
2023-03-12 17:14:26 -0700

    Update: Working on the Project Webpage

--- styles/contact.js
+++ styles/contact.js
@@ -1,15 +1,22 @@
 let form = document.querySelector(".form");
 
 form.addEventListener("submit", (event) => {
-    if (form.name.value == "" | form.email.value == "" | form.feedback.value==""){
-        console.warn("You must enter all the necessary information to submit this form");
-        event.preventDefault();
-        return;
-    }
-    console.log ("---------------------- Feedback Submission ----------------------");
-    console.log (`Name: ${form.name.value}`);
-    console.log (`Email: ${email.value}`);
-    console.log (`Name: ${feedback.value}`);
+  if (
+    (form.name.value == "") |
+    (form.email.value == "") |
+    (form.feedback.value == "")
+  ) {
+    console.warn(
+      "You must enter all the necessary information to submit this form"
+    );
     event.preventDefault();
-
-})
+    return;
+  }
+  console.log(
+    "---------------------- Feedback Submission ----------------------"
+  );
+  console.log(`Name: ${form.name.value}`);
+  console.log(`Email: ${email.value}`);
+  console.log(`Name: ${feedback.value}`);
+  event.preventDefault();
+});
